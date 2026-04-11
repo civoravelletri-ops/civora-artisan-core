5239e5f31f83d767af1a36423250665f721e4974
civoravelletri-ops <civora.velletri@gmail.com>
2026-04-11 10:03:56 +0200

    Update generate-look.js

--- api/generate-look.js
+++ api/generate-look.js
@@ -56,7 +56,7 @@ module.exports = async (req, res) => {
         const location = 'us-central1'; 
         
         // IL NUOVO MOTORE UNIFICATO DI GOOGLE
-        const modelId = 'imagen-4.0-generate-001'; 
+        const modelId = 'gemini-2.5-flash-image'; 
 
         // Il nuovo URL per Gemini usa "generateContent" invece di "predict"
         const url = `https://${location}-aiplatform.googleapis.com/v1/projects/${projectId}/locations/${location}/publishers/google/models/${modelId}:generateContent`;
