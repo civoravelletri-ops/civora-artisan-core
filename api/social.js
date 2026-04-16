0199a852a1d56c045392cee161c14515316f50de
civoravelletri-ops <civora.velletri@gmail.com>
2026-04-16 13:15:15 +0200

    Update social.js

--- api/social.js
+++ api/social.js
@@ -1,10 +1,10 @@
 export default async function handler(req, res) {
     // Permetti al tuo sito di chiamare questa funzione (CORS)
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-
+        res.setHeader('Access-Control-Allow-Origin', 'https://localmente-site.web.app'); 
+        res.setHeader('Access-Control-Allow-Origin', 'https://localmente-v3-core.firebaseapp.com');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+        res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
